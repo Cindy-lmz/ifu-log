8fd1393aadf69df559024ef290811b1b1678a9da
cindy <cindyli@topichina.com.cn>
2021-02-04 12:30:19 +0800

    no message

--- Log.php
+++ Log.php
@@ -80,22 +80,16 @@ class Log
         // 日志信息封装
         $time = time();
         $message = [];
-        foreach ($log as $msg) {
+        foreach ($log as $key => $msg) {
             if (!is_string($msg)) {
                 $msg = var_export($msg, true);
             }
             $logItem = new \Aliyun_Log_Models_LogItem();
             $logItem->setTime($time);
-            $runtime = (number_format(microtime(true), 8, '.', '') - THINK_START_TIME) ?: 0.00000001;
+            $runtime = (number_format(microtime(true), 8, '.', '') - App::getBeginTime()) ?: 0.00000001;
             $reqs = number_format(1 / number_format($runtime, 8), 2);
-            $contents =[
-                'level' => $msg['level'], 
-                'apiUrl' => $msg['apiUrl'], 
-                'urlData' => $msg['urlData'], 
-                'curl_error' => $msg['curl_error'], 
-                'differenct_time' =>  number_format($runtime, 6), 
-                'response' => $msg['response']
-            ];
+            $contents = [$key=>$msg];
+            $contents =['differenct_time' =>  number_format($runtime, 6)];
             $logItem->setContents($contents);
             $message[] = $logItem;
         }
@@ -116,7 +110,6 @@ class Log
         $client = new \Aliyun_Log_Client(config('log.aliyun_log_endpoint'), config('log.aliyun_log_access_key_id'), config('log.aliyun_log_access_key_secret'));
         $req = new \Aliyun_Log_Models_PutLogsRequest(config('log.aliyun_log_project'), config('log.aliyun_log_logstore'), config('log.aliyun_log_topic'), config('log.aliyun_log_source'), $message);
         $client->putLogs($req);
-        // var_dump($req);exit();
         return true;
     }
 }
